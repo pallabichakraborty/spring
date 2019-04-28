a80b093707b7a61559f6e7ad0a3fa20bac84c2ae
Pallabi Chakraborty <pallavichakraborty@yahoo.com>
2019-04-28 14:38:23 -0500

    Added Rest Component Details

---
+++ spring-boot-rest-example/src/main/java/com/restservice/example/springbootrestexample/user/UserDaoService.java
@@ -0,0 +1,51 @@
+package com.restservice.example.springbootrestexample.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	private Integer usersCount=3;
+	
+	private static List<User> users=new ArrayList<User>();
+	
+	static {
+		users.add(new User(1, "Someone1", new Date()));
+		users.add(new User(2, "Someone2", new Date()));
+		users.add(new User(3, "Someone3", new Date()));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User save(User user) 
+	{
+		if(user.getId()==null)
+		{
+			user.setId(++usersCount);
+		}
+		
+		users.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		
+		for(User user:users)
+		{
+			if(user.getId()==id)
+			{
+				return user;
+			}
+		}
+		
+		return null;
+		
+	}
+
+}
---
+++ spring-boot-rest-example/src/main/java/com/restservice/example/springbootrestexample/user/UserResource.java
@@ -0,0 +1,50 @@
+package com.restservice.example.springbootrestexample.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	//retrieveAllUsers
+	@GetMapping(path="/users")
+	public List<User> retrieveAllUsers()
+	{
+		return service.findAll();
+	}
+	
+	//retrieveUser(int id)
+	@GetMapping(path="/users/{id}")
+	public User retrieveUser(@PathVariable int id)
+	{
+		return service.findOne(id);
+	}
+
+	//input - details of user
+	//output -Created and return the created URI
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user)
+	{
+		User savedUser=service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
+	}
+
+}
