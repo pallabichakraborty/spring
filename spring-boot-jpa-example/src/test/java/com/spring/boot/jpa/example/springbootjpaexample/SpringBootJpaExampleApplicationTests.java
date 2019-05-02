558470a390bc875202d8fdef6122670cae298067
Pallabi Chakraborty <pallavichakraborty@yahoo.com>
2019-05-02 09:55:53 -0500

    Start JPA

---
+++ spring-boot-jpa-example/src/test/java/com/spring/boot/jpa/example/springbootjpaexample/SpringBootJpaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.boot.jpa.example.springbootjpaexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJpaExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
